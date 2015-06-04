e9582aba2794fe049c05e6e5597971d65c0c3251
Adriano Soares <gsoares.adriano@gmail.com>
2015-06-04 20:11:45 -0300

    analog stick added

--- MC2/Scenes/GameScene.swift
+++ MC2/Scenes/GameScene.swift
@@ -8,10 +8,24 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, AnalogStickProtocol {
+    
+    let moveAnalogStick: AnalogStick = AnalogStick()
+    let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        
+        let bgDiametr: CGFloat = 120
+        let thumbDiametr: CGFloat = 60
+        moveAnalogStick.bgNodeDiametr = bgDiametr
+        moveAnalogStick.thumbNodeDiametr = thumbDiametr
+        moveAnalogStick.delegate = self
+        moveAnalogStick.hidden = true
+        moveAnalogStick.alpha = 0.25
+        moveAnalogStick.position = CGPointMake(-100, -100)
+        self.addChild(moveAnalogStick)
+        
         myLabel.text = "Hello, World!";
         myLabel.fontSize = 65;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
@@ -19,26 +33,41 @@ class GameScene: SKScene {
         self.addChild(myLabel)
     }
     
+    
+    
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
+        super.touchesBegan(touches, withEvent: event)
+
+
         
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            moveAnalogStick.position = location
+            moveAnalogStick.hidden = false
         }
+        moveAnalogStick.touchesBegan(touches, withEvent: event)
+        moveAnalogStick.touchesMoved(touches, withEvent: event)
+
     }
    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        super.touchesMoved(touches, withEvent: event)
+        moveAnalogStick.touchesMoved(touches, withEvent: event)
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        super.touchesEnded(touches, withEvent: event)
+        moveAnalogStick.touchesEnded(touches, withEvent: event)
+        moveAnalogStick.position = CGPointMake(-100, -100)
+        moveAnalogStick.hidden = true
+    }
+    
+    func moveAnalogStick(analogStick: AnalogStick, velocity: CGPoint, angularVelocity: Float) {
+        
+    }
+    
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
