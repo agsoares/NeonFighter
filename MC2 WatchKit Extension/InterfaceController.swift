028443575fcf43b48f1fd4ec348b10ba95aff8a7
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2015-07-16 13:44:38 -0300

    adjusted watch App

--- MC2 WatchKit Extension/InterfaceController.swift
+++ MC2 WatchKit Extension/InterfaceController.swift
@@ -12,6 +12,9 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    var score = 0;
+    
+    @IBOutlet weak var lblBestScore: WKInterfaceLabel!
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
@@ -19,8 +22,14 @@ class InterfaceController: WKInterfaceController {
     }
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        var dict = NSDictionary(dictionary: ["action" : "getBestScores"])
+        WKInterfaceController.openParentApplication(["action" : "getBestScores"], reply: { (obj: [NSObject : AnyObject]!, error: NSError!) -> Void in
+            if let dic = obj as NSDictionary!{
+                self.score = (dic.valueForKey("bestScore")) as! Int;
+                self.lblBestScore.setText("Your Best Score is \(self.score)")
+            }
+        })
     }
 
     override func didDeactivate() {
